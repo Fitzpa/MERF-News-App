d8eec005ea645b4dd5911420e54827594937d783
Louie Fitzpatrick <fitz4492@gmail.com>
2019-06-22 02:51:25 -0500

    Added firebase functions and deployed first function

--- src/components/Link/LinkItem.js
+++ src/components/Link/LinkItem.js
@@ -17,7 +17,8 @@ function LinkItem({ link, index, showCount, history }) {
           const previousVotes = doc.data().votes;
           const vote = { votedBy: { id: user.uid, name: user.displayName } };
           const updatedVotes = [...previousVotes, vote];
-          voteRef.update({ votes: updatedVotes });
+          const voteCount = updatedVotes.length;
+          voteRef.update({ votes: updatedVotes, voteCount });
         }
       });
     }
@@ -47,11 +48,13 @@ function LinkItem({ link, index, showCount, history }) {
       </div>
       <div className="ml1">
         <div>
-          {link.description}{" "}
+          <a href={link.url} className="black no-underline">
+            {link.description}
+          </a>{" "}
           <span className="link">({getDomain(link.url)})</span>
         </div>
         <div className="f6 lh-copy gray">
-          {link.votes.length} votes by {link.postedBy.name}{" "}
+          {link.voteCount} votes by {link.postedBy.name}{" "}
           {distanceInWordsToNow(link.created)}
           {" | "}
           <Link to={`/link/${link.id}`}>
